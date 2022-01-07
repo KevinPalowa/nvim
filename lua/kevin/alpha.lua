62988d025493e64f5061eaa108f5cccd97136edd
Kevin Palowa <kevinpalowa@gmail.com>
2022-01-07 15:27:01 +0700

    change alpha footer

--- lua/kevin/alpha.lua
+++ lua/kevin/alpha.lua
@@ -28,7 +28,7 @@ local function footer()
 	-- local fortune = handle:read("*a")
 	-- handle:close()
 	-- return fortune
-	return "chrisatmachine.com"
+	return "Kevin Palowa"
 end
 
 dashboard.section.footer.val = footer()
