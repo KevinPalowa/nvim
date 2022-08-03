89983812e3eb4eef5f270224a8d06a8a52f8b085
Kevin Palowa <kevinpalowa@gmail.com>
2022-08-03 12:25:33 +0700

    Change toggleterm config and whichkey

--- lua/kevin/whichkey.lua
+++ lua/kevin/whichkey.lua
@@ -107,7 +107,7 @@ local mappings = {
 
 	g = {
 		name = "Git",
-		g = { "<cmd>TermExec cmd='lazygit'<CR>", "Lazygit" },
+		g = { "<cmd>lua _LAZYGIT_TOGGLE()<CR>", "Lazygit" },
 		j = { "<cmd>lua require 'gitsigns'.next_hunk()<cr>", "Next Hunk" },
 		k = { "<cmd>lua require 'gitsigns'.prev_hunk()<cr>", "Prev Hunk" },
 		l = { "<cmd>lua require 'gitsigns'.blame_line()<cr>", "Blame" },
