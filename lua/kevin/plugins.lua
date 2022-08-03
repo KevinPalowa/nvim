ec07a232c4a3e4714e04cd0a68ec85c9b40d0c94
Kevin Palowa <kevinpalowa@gmail.com>
2022-08-03 12:25:03 +0700

    Add colorscheme (synthwave84)

--- lua/kevin/plugins.lua
+++ lua/kevin/plugins.lua
@@ -82,6 +82,7 @@ return packer.startup(function(use)
 	use("morhetz/gruvbox")
 	use("glepnir/zephyr-nvim")
 	use({ "catppuccin/nvim" })
+	use("lunarvim/synthwave84.nvim")
 	-- cmp plugins
 	use("hrsh7th/nvim-cmp") -- The completion plugin
 	use("hrsh7th/cmp-buffer") -- buffer completions
