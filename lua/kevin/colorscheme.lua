bf7ab296367691459f596bc57b36f3ef7d077fa1
Kevin Palowa <kevinpalowa@gmail.com>
2022-09-21 07:47:36 +0700

    Using thebest color scheme

--- lua/kevin/colorscheme.lua
+++ lua/kevin/colorscheme.lua
@@ -1,6 +1,18 @@
-local colorscheme = "everblush"
+--[[ local colorscheme = "everblush"
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
 	vim.notify("colorscheme " .. colorscheme .. " not found!")
 	return
-end
+end ]]
+-- Lua
+require("onedark").setup({
+	style = "deep",
+	code_style = {
+		comments = "italic",
+		keywords = "none",
+		functions = "none",
+		strings = "none",
+		variables = "none",
+	},
+})
+require("onedark").load()
