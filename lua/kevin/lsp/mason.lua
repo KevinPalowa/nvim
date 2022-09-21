1836474e9dd249fe4e71b9e3155f85cfcf7ddee2
Kevin Palowa <kevinpalowa@gmail.com>
2022-09-21 11:07:56 +0700

    Migrate fully to mason

--- lua/kevin/lsp/mason.lua
+++ lua/kevin/lsp/mason.lua
@@ -8,35 +8,8 @@ if not status_ok_1 then
 	return
 end
 
-local servers = {
-	"cssls",
-	"cssmodules_ls",
-	"emmet_ls",
-	"html",
-	"jsonls",
-	"sumneko_lua",
-	"tsserver",
-	"pyright",
-	"yamlls",
-	"bashls",
-}
-
-local settings = {
-	ui = {
-		border = "rounded",
-		icons = {
-			package_installed = "◍",
-			package_pending = "◍",
-			package_uninstalled = "◍",
-		},
-	},
-	log_level = vim.log.levels.INFO,
-	max_concurrent_installers = 4,
-}
-
-mason.setup(settings)
+mason.setup()
 mason_lspconfig.setup({
-	ensure_installed = servers,
 	automatic_installation = true,
 })
 
@@ -44,48 +17,27 @@ local lspconfig_status_ok, lspconfig = pcall(require, "lspconfig")
 if not lspconfig_status_ok then
 	return
 end
+mason_lspconfig.setup_handlers({
+	-- This is a default handler that will be called for each installed server (also for new servers that are installed during a session)
+	function(server_name)
+		local opts = {
+			on_attach = require("kevin.lsp.handlers").on_attach,
+			capabilities = require("kevin.lsp.handlers").capabilities,
+		}
 
-local opts = {}
-
-for _, server in pairs(servers) do
-	opts = {
-		on_attach = require("kevin.lsp.handlers").on_attach,
-		capabilities = require("kevin.lsp.handlers").capabilities,
-	}
-
-	server = vim.split(server, "@")[1]
-
-	if server == "jsonls" then
-		local jsonls_opts = require("kevin.lsp.settings.jsonls")
-		opts = vim.tbl_deep_extend("force", jsonls_opts, opts)
-	end
-
-	if server == "yamlls" then
-		local yamlls_opts = require("kevin.lsp.settings.yamlls")
-		opts = vim.tbl_deep_extend("force", yamlls_opts, opts)
-	end
-
-	if server == "sumneko_lua" then
-		local l_status_ok, lua_dev = pcall(require, "lua-dev")
-		if not l_status_ok then
-			return
+		if server_name == "emmet_ls" then
+			local emmetls_opts = require("kevin.lsp.settings.emmetls")
+			opts = vim.tbl_deep_extend("force", emmetls_opts, opts)
 		end
-		-- local sumneko_opts = require "user.lsp.settings.sumneko_lua"
-		-- opts = vim.tbl_deep_extend("force", sumneko_opts, opts)
-		-- opts = vim.tbl_deep_extend("force", require("lua-dev").setup(), opts)
-		local luadev = lua_dev.setup({
-			--   -- add any options here, or leave empty to use the default settings
-			-- lspconfig = opts,
-			lspconfig = {
-				on_attach = opts.on_attach,
-				capabilities = opts.capabilities,
-				--   -- settings = opts.settings,
-			},
-		})
-		lspconfig.sumneko_lua.setup(luadev)
-		goto continue
-	end
+		if server_name == "jsonls" then
+			local jsonls_opts = require("kevin.lsp.settings.jsonls")
+			opts = vim.tbl_deep_extend("force", jsonls_opts, opts)
+		end
+		if server_name == "sumneko_lua" then
+			local sumneko_opts = require("kevin.lsp.settings.sumneko_lua")
+			opts = vim.tbl_deep_extend("force", sumneko_opts, opts)
+		end
 
-	lspconfig[server].setup(opts)
-	::continue::
-end
+		lspconfig[server_name].setup(opts)
+	end,
+})
