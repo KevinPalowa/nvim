3f4d0e35185562bfa49529313faf881d5d732015
Kevin Palowa <kevinpalowa@gmail.com>
2022-10-01 10:40:57 +0700

    Change separator and set globalstatus to true

--- lua/kevin/lualine.lua
+++ lua/kevin/lualine.lua
@@ -1,16 +1,16 @@
 require("lualine").setup({
 	options = {
 		icons_enabled = true,
-		theme = "auto",
-		component_separators = { left = "", right = "" },
-		section_separators = { left = "", right = "" },
+		theme = "catppuccin",
+		component_separators = { left = "|", right = "|" },
+		section_separators = { left = "", right = "" },
 		disabled_filetypes = {
 			statusline = {},
 			winbar = {},
 		},
 		ignore_focus = {},
 		always_divide_middle = true,
-		globalstatus = false,
+		globalstatus = true,
 		refresh = {
 			statusline = 1000,
 			tabline = 1000,
@@ -21,8 +21,8 @@ require("lualine").setup({
 		lualine_a = { "mode" },
 		lualine_b = { "branch", "diff", "diagnostics" },
 		lualine_c = { "filename" },
-		lualine_x = { "encoding", "fileformat", "filetype" },
-		lualine_y = { "progress" },
+		lualine_x = { "encoding", "filetype" },
+		lualine_y = {},
 		lualine_z = { "location" },
 	},
 	inactive_sections = {
